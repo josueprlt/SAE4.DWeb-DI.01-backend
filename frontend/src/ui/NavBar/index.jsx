dd017827428334b5c329327a03422d6aa4dc6433
josueprlt <148058543+josueprlt@users.noreply.github.com>
2024-03-18 17:30:28 +0100

    update

--- frontend/src/ui/NavBar/index.jsx
+++ frontend/src/ui/NavBar/index.jsx
@@ -1,32 +1,13 @@
-import {Link} from 'react-router-dom';
-
+import { Link } from "react-router-dom";
+import logo from '../../assets/icon_logo.svg';
 
 export default function NavBar() {
-
-    return (
-
-<nav
-  className="flex-no-wrap relative flex w-full items-center justify-between bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:flex-wrap lg:justify-start lg:py-4">
-  <div className="flex w-full flex-wrap items-center justify-between px-3"> 
-   
-    <div className="list-style-none me-auto flex flex-col ps-0 lg:flex-row text-white space-x-10">
-  
-        <div className="list-style-none me-auto flex flex-col ps-0 lg:flex-row text-white">
-          <Link className="mb-4 lg:mb-0 lg:pe-2 text-[#BBA]" to="/crash">Our Teams </Link>
-          <Link className="mb-4 lg:mb-0 lg:pe-2 hover:text-[#BBA] focus:text-[#BB0]" to="/team/sales">Sales</Link>
-          <Link className="mb-4 lg:mb-0 lg:pe-2 hover:text-[#BBA] focus:text-[#BB0]" to="/team/webdesign">Webdesign</Link>
-          <Link className="mb-4 lg:mb-0 lg:pe-2 hover:text-[#BBA] focus:text-[#BB0]" to="/team/development">Development</Link>
-        </div>
-        <div className="list-style-none me-auto flex flex-col ps-0 lg:flex-row text-white space-x-10">
-          <Link className="ml-15 mb-4 lg:mb-0 lg:pe-2 hover:text-[#BBA] focus:text-[#BB0]" to="/buy">Buy</Link>
-          <Link className="mb-4 lg:mb-0 lg:pe-2 hover:text-[#BBA] focus:text-[#BB0]" to="/about">?</Link>
-        </div>
-          
-    </div>
-    
-  </div>
-</nav>
-    );
-
+  return (
+    <nav className="flex-no-wrap shadow-dark-mild relative flex w-full items-center justify-between border-t-8 border-[#007AFF] bg-[#000E29]">
+      
+      <Link className="mb-4 text-[#BBA] lg:mb-0 lg:pe-2" to="/crash">
+      <img src={logo} alt="Logo" />
+      </Link>
+    </nav>
+  );
 }
-
